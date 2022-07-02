bdeb1fc0bb9423405dd85a7f38c0e6d98f36dc96
dairin007 <33408887+dairin007@users.noreply.github.com>
2022-07-03 02:01:37 +0900

    first commit

---
+++ test/starttest.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+MINECRAFT_VERSION=1.19
+mkdir -p data
+echo "start server"
+
+# TODO: download plugin
+
+docker-compose up -d
+sleep 20
+docker-compose ps
+
+if [ ! -z "$(docker-compose ps | grep 'Exit 1')" ]; then
+  echo "error: service is down"
+  exit 1
+fi
+
+# wait start loop
+SECONDS=0
+while true
+do
+	sleep 1
+	MCSTATUS_JSON=$(mcstatus localhost:${export_port_tcp} json)
+	MCSTATUS_ONLINE=$(echo ${MCSTATUS_JSON} | jq .online)
+	if [ "${MCSTATUS_ONLINE}" == 'true' ]; then
+		break
+	fi
+	echo "waiting...${SECONDS}"
+	if [ $SECONDS -gt 60 ]; then
+		echo "timeout"
+		break
+	fi
+done
+
+docker-compose logs
+echo "${MCSTATUS_JSON}"
+MCSTATUS_VERSION=$(echo ${MCSTATUS_JSON} | jq .version)
+
+if [ "${MCSTATUS_VERSION}" != "\"Paper ${MINECRAFT_VERSION}\"" ]; then
+  echo "Minecraft version mismatch: ${MCSTATUS_VERSION}"
+  exit 1
+fi
+
+docker-compose down
+
+exit 0
