0ae7294ce41f50c4db3885ad94297f071eefc2f6
dairin007 <33408887+dairin007@users.noreply.github.com>
2022-07-04 19:20:12 +0900

    listed plugin

---
+++ download_plugin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Plugin Tuple
+# "plugin_name plugin_version plugin_url"
+PLUGINS=(
+ "PlayPro/CoreProtect 21.2 https://github.com/PlayPro/CoreProtect/releases/download/"
+ "DiscordSRV/DiscordSRV 1.25.1 https://github.com/PlayPro/CoreProtect/releases/download"
+ "GeyserMC/Floodgate 70 https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master"
+ "GeyserMC/Geyser 1133 https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master"
+ "Camotoy/GeyserSkinManager 1.6 https://github.com/Camotoy/GeyserSkinManager/releases"
+ "sladkoff/minecraft-prometheus-exporter 2.5.0 https://github.com/sladkoff/minecraft-prometheus-exporter/releases"
+ "ViaVersion/ViaBackwards 4.3.1 https://github.com/ViaVersion/ViaBackwards/releases"
+ "ViaVersion/ViaBackwards 4.3.0 https://github.com/ViaVersion/ViaBackwards/releases"
+)
+
+
+
